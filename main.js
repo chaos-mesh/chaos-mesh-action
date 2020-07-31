22f75c0ce63597a9d094841b774a5c4f2b119b12
xiang <xiang13225080@163.com>
2020-07-31 10:34:48 +0800

    clone actions to get go file

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const spawnSync = require('child_process').spawnSync;
 const path = require("path");
 
-ACTION_PATH = __dirname
-
 const proc = spawnSync('bash', [path.join(__dirname, 'main.sh')], {stdio: 'inherit'});
 process.exit(proc.status)
--- main.sh
+++ main.sh
@@ -13,12 +13,14 @@ CHAOS_KIND=${CHAOS_KIND:="NULL"}
 #cat ${HOME}/.kube/config
 
 ls -l ./
-echo "action path:" $ACTION_PATH
 
-#ifconfig
+git clone https://github.com/WangXiangUSTC/chaos-mesh-actions.git
+go run chaos-mesh-actions/utils/generate_config.go --chaos-type ${CHAOS_KIND}
+
 
 git clone https://github.com/chaos-mesh/chaos-mesh.git
 cd chaos-mesh
+mv ../chaos.yaml ./
 
 helm version
 kubectl version
@@ -27,6 +29,6 @@ kubectl create ns chaos-testing
 helm install chaos-mesh helm/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
 kubectl get pods --namespace chaos-testing -l app.kubernetes.io/instance=chaos-mesh
 
-go run $ACTION_PATH/utils/generate_config.go --chaos-type ${CHAOS_KIND}
+
 
 kubectl apply -f chaos.yaml
