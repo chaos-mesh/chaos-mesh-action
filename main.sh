8deb02dc2b86bcfab7851bdcc46afe08d1329ffd
xiang <xiang13225080@163.com>
2020-07-31 11:22:30 +0800

    add sleep

--- main.sh
+++ main.sh
@@ -18,4 +18,5 @@ kubectl apply -f ./manifests/crd.yaml
 kubectl create ns chaos-testing
 helm install chaos-mesh helm/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
 kubectl get pods --namespace chaos-testing -l app.kubernetes.io/instance=chaos-mesh
+sleep 5
 kubectl apply -f chaos.yaml
